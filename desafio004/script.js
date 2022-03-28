d5dd064f222c8269e0bdf3a3940584dcd1e27877
Alonso Martins <66711870+Martins-Alonso@users.noreply.github.com>
2022-03-28 16:19:53 -0400

    Ajustando os detalhes
    Foi tirado o main.

--- desafio004/script.js
+++ desafio004/script.js
@@ -1,5 +1,13 @@
 function comecar(){
- let nome =  prompt("Qual é o seu nome?")
- let resultado = document.querySelector("resultado")
+    
+    let nome = prompt("Qual é o seu nome ?")
+
+    if(nome === "") {
+        alert("Campo nome vazio")
+    }else {
+        let res = document.getElementById("resultado")
+        let novoP = document.createElement('p')
+        res.appendChild(novoP).textContent = `Olá, ${nome} É um prazer te conhecer`
+    }
 }
 
