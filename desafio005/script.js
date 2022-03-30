c1212f749932e1fbb6ba4a182b6120d02c95b56e
Alonso Martins <66711870+Martins-Alonso@users.noreply.github.com>
2022-03-29 21:56:42 -0400

    Trabalhando com números
    Neste desafio, foi criada uma função que recebe um número e multiplica esse número por 2. Após isso essa mesma função divide o mesmo número por 2

---
+++ desafio005/script.js
@@ -0,0 +1,12 @@
+alert("Seja bem-vindo(a) ao meu site")
+
+function calcular(){
+    let numero = Number(prompt("Digite o número"))
+    let dobro = numero * 2;
+    let metade = numero / 2;
+    
+    let resultado = document.getElementById("resultado")
+    let novoParagrafo = document.createElement("p")
+    resultado.appendChild(novoParagrafo)
+    novoParagrafo.textContent = `O dobro de ${numero} é: ${dobro} e a metade é: ${metade}`
+}
